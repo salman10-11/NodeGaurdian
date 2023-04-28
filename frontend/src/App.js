ac10328a956e4273fd2ea722e98f4e5d5e2fe5ee
salman10-11 <mohammad.salman.bca2020@bbdu.ac.in>
2023-04-28 11:23:40 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Main from './components/main';
+import Login from './components/main/Login';
+import Home from './components/main/Home';
+import Signup from './components/main/Signup';
+import Navbar from './components/main/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+
+          <Route path="main" element={<Main />} >
+            <Route path="home" element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="navbar" element={<Navbar />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
